4d87baa41baf9838ae6b2ed2a12200e0513c5ef7
MandisaT <mandisa.thomas1@gmail.com>
2016-08-19 02:33:49 -0400

    adding jquery and social media icons

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	
+
+	$(".box").hover(function(){
+	$(this).find(".projectoverlay").fadeIn(100);
+	},
+
+	function(){
+		$(this).find(".projectoverlay").fadeOut(100);
+	});
